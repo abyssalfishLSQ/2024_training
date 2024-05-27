beb17d732c13854a1aff1b4adfff717f87b85f56
abyssalfish_LSQ <12923533+abyssalfish-lsq@user.noreply.gitee.com>
2024-05-27 14:33:22 +0800

    Collector、Server已经跑通，但Server仍然需要调试

--- Collector/src/main/java/com/xiaomi/collector/CollectorApplication.java
+++ Collector/src/main/java/com/xiaomi/collector/CollectorApplication.java
@@ -1,20 +1,32 @@
+package com.xiaomi.collector;
+
+import com.xiaomi.collector.common.Result;
+import com.xiaomi.collector.entity.Metric;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
-import java.util.*;
 
-public class Collector {
+import java.util.ArrayList;
+import java.util.List;
 
+@SpringBootApplication
+@EnableScheduling
+public class CollectorApplication {
+
+    @Autowired
+    private RestTemplate restTemplate;
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                uploadMetrics();
-            }
-        }, 0, 60000); // 每分钟执行一次
+        SpringApplication.run(CollectorApplication.class, args);
     }
 
-    public static void uploadMetrics() {
+    @Scheduled(fixedRate = 5000)
+    public void uploadMetrics() {
+        System.out.println("****************************************\n");
+
         // 模拟采集CPU和内存利用率数据
         List<Metric> metrics = new ArrayList<>();
         // 假设获取了CPU利用率和内存利用率的值
@@ -24,16 +36,17 @@ public class Collector {
         metrics.add(memMetric);
 
         // 发送HTTP POST请求上传数据
-        RestTemplate restTemplate = new RestTemplate();
         String serverUrl = "http://localhost:9092/api/metric/upload";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<List<Metric>> request = new HttpEntity<>(metrics, headers);
-        ResponseEntity<Response> responseEntity = restTemplate.postForEntity(serverUrl, request, Response.class);
+        ResponseEntity<Result> responseEntity = restTemplate.postForEntity(serverUrl, request, Result.class);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             System.out.println("Metrics uploaded successfully");
         } else {
             System.out.println("Failed to upload metrics");
         }
     }
+
+
 }
